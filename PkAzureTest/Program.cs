227ef86d3777100bdb40a00ee5b410af04ac139a
Patrick Keeney <patrick.keeney@steeldynamics.com>
2025-01-13 10:38:43 -0500

    Added username display code to test authentication

--- PkAzureTest/Program.cs
+++ PkAzureTest/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddCascadingAuthenticationState();
 
 var app = builder.Build();
 
